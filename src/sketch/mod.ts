bf3f7d4b18f5d412a6df6ecc3b0edc34c0332243
chenshenhai <cshenhai@gmail.com>
2019-04-06 23:55:45 +0800

    feat: init sketch

--- src/sketch/mod.ts
+++ src/sketch/mod.ts
@@ -3,30 +3,68 @@ export interface SketchOptions {
   width: number,
   height: number,
   layerCount: number,
-  container: HTMLCanvasElement,
+  container: HTMLDivElement,
+}
+
+function mergeCSS2Style(css: object) {
+  let result = '';
+  let resultList = [];
+  const keys = Object.keys(css);
+  keys.forEach((name: string) => {
+    if (typeof name === 'string') {
+      const value: string = css[name] || '';
+      if (typeof value === 'string') {
+        resultList.push(`${name}:${value}`);
+      }
+    }
+  });
+  result = resultList.join(';');
+  return result;
 }
 
 class Sketch {
 
-  private _width: number;
-  private _height: number;
-  private _layerCount: number;
+  private _width: number = 200;
+  private _height: number = 200;
+  private _layerCount: number = 1;
+  private _container: HTMLDivElement;
+  private _canvasStock: HTMLCanvasElement[] = [];
 
   constructor(opts: SketchOptions) {
-    console.log('opts = ', opts);
-    console.log('this = ', this);
     this._width = opts.width;
     this._height = opts.height;
     this._layerCount = opts.layerCount;
+    this._container = opts.container
     this._initSketch();
   }
 
   private _initSketch() {
-    console.log('hello sketch!')
+    const container: HTMLDivElement = this._container;
+    const width = this._width;
+    const height = this._height;
+    const style = mergeCSS2Style({
+      width: `${width}px`,
+      height: `${height}px`,
+      position: 'relative',
+    });
+    container.setAttribute('style', style);
+    const count = this._layerCount;
+    for (let i = 0; i < count; i ++) {
+      const canvas = document.createElement('canvas');
+      canvas.width = width;
+      canvas.height = height;
+      canvas.setAttribute('style', mergeCSS2Style({
+        width: `${width}px`,
+        height: `${height}px`,
+        position: 'absolute',
+      }));
+      this._canvasStock.push(canvas);
+      container.appendChild(canvas);
+    }
   }
 
-  getLayer() {
-
+  getLayer(index: number) {
+    return this._canvasStock[index];
   }
 
 }
