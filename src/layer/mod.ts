ecb3653314cc8927c4cef53a84fd2eb49882e6e4
chenshenhai <cshenhai@gmail.com>
2019-04-12 00:10:32 +0800

    feat: update layer

--- src/layer/mod.ts
+++ src/layer/mod.ts
@@ -1,5 +1,12 @@
+interface ContextDrawAction {
+  method: string,
+  args: any[],
+}
+
+
 class Layer {
   private _context: CanvasRenderingContext2D;
+  private _contextDrawActionList: ContextDrawAction[] = [];
 
   constructor(context: CanvasRenderingContext2D) {
     this._context = context;
@@ -7,6 +14,31 @@ class Layer {
 
   getLayerContext() {
     return this._context;
+  }
+
+  pushContextDrawAction(method, args) {  
+    this._contextDrawActionList.push({
+      method,
+      args,
+    })
+  }
+
+  executeDrawAction() {
+    const list: ContextDrawAction[] = this._contextDrawActionList;
+    const context = this._context;
+    for (let i = 0; i < list.length; i++) {
+      const action = list[i];
+      if (typeof action.method !== 'string') {
+        continue;
+      }
+      if (Array.isArray(action.args) !== true) {
+        continue;
+      }
+      if (typeof context[action.method] !== 'function') {
+        return;
+      }
+      context[action.method](...action.args);
+    }
   }
 }
 
