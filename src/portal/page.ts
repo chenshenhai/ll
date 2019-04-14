842394cfb9b0d970a18857f0b34095480c18c3ef
chenshenhai <cshenhai@gmail.com>
2019-04-14 12:29:51 +0800

    feat: add portal btn style

--- src/portal/page.ts
+++ src/portal/page.ts
@@ -1,3 +1,39 @@
+import Logox from './../logox/mod.ts';
 import './less/index.less';
+
+const container = document.getElementById('logox-sketchpad');
+const lx = new Logox({
+  width: 500,
+  height: 500,
+  layerCount: 2,
+  container,
+});
+
+var sketchSchema = {
+  name: '',
+  layerList: [
+    { 
+      name: '',
+      drawActionList: [{
+        method: 'fillStyle',
+        args: 'red',
+      }, {
+        method: 'fillRect',
+        args: [50, 100, 200, 100],
+      }],
+    },
+    {
+      name: '',
+      drawActionList: [{
+        method: 'fillStyle',
+        args: 'blue',
+      }, {
+        method: 'fillRect',
+        args: [100, 150, 200, 100],
+      }],
+    }
+  ]
+}
+lx.render(sketchSchema);
 
 console.log('hello logox-portal');
