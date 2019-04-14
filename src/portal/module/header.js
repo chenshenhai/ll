25e1d01fc3fd6bf2b45dee1aae2de3def9f610d0
chenshenhai <cshenhai@gmail.com>
2019-04-14 21:38:22 +0800

    feat: update portal style

---
+++ src/portal/module/header.js
@@ -0,0 +1,10 @@
+const $header = document.getElementById('J_Logox_Header');
+const $btnSketch = document.getElementById('J_Btn_SketchMode');
+const $btnSchema = document.getElementById('J_Btn_SchemaMode');
+const header = {
+  init() {
+
+  }
+}
+
+export default header;
